aa43aca04486986e5b27cc7630f69c3b699ff62a
Tumelo <tumie201511@gmail.com>
2023-02-11 17:12:53 +0200

    added a loading spinner component

---
+++ src/common/widgets/Spinner.vue
@@ -0,0 +1,17 @@
+<!-- <script>
+
+import 
+    PulseLoader      
+from 'vue-spinner/src/PulseLoader.vue';
+
+export default {
+    components: {
+        PulseLoader
+    }
+}
+</script>
+
+<template>
+    <PulseLoader />
+</template>
+ -->
