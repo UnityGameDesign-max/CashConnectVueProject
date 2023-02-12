42f0ae24c5898707fb62fd2e76218ab837ba142e
Tumelo <tumie201511@gmail.com>
2023-02-12 18:36:30 +0200

    added a helperFunction that and components for home component

---
+++ src/common/helpers/splitRemoteUriHelper.js
@@ -0,0 +1,12 @@
+export function splitRemoteUri(dbObject){
+    if(
+        !dbObject || 
+        typeof(dbObject) !== "object"
+    ){
+        return;
+    }
+    const remoteHostUri = 
+    `http://${dbObject.host}:${dbObject.port}`;
+
+    return remoteHostUri;
+}
---
+++ src/ui/home/components/Header.vue
@@ -0,0 +1,36 @@
+<script>
+
+export default{
+    props: {
+        user: {
+            type: String,
+            required: true,
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class='header'>
+        <p class='header__user'>Welcome 
+            <span class='header__username'>{{ user }}</span>
+        </p>
+    </div>
+</template>
+
+<style scoped>
+    .header{
+        padding: 2vh 3vw;
+        box-shadow: 0 3px 8px 0 var(--Transparent);
+        transition: 0.3s;
+        background-color: var(--CharcoalBlack);
+    }
+    .header__user{
+        color: var(--White);
+    }
+    .header__username{
+        font-weight: 600;
+        color: var(--LightGrey);
+    }
+</style>
