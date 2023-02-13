a5de8c4bb7e41a1819b41e52586a689f1e271b44
Tumelo <tumie201511@gmail.com>
2023-02-13 07:17:37 +0200

    updated the file name from loginProvider to authProvider

---
+++ src/providers/authProvider.js
@@ -0,0 +1,36 @@
+import {
+    Environment
+} from '@/common';
+
+import { 
+    Messages 
+} from '@/common';
+
+import { 
+    errorHandlingToProvideResults
+} from '@/common/helpers/errorHandlingHelper';
+
+const validateUserUri = `${Environment.apiHost}/login`;
+
+export async function executeLoginUser(credentials){
+    try {
+        const response = await fetch(`${validateUserUri}`, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(credentials)
+        })
+        const data = await response.json();
+        errorHandlingToProvideResults(response, Messages);
+
+        return {
+            status: response.status,
+            response: data
+        }
+    }
+    catch(error){
+        return error.message;
+    }
+}
+
